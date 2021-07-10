c1faf65b92c10610c0b2a9148718b5d51dcbcf23
Partha De <47812891+parthade92@users.noreply.github.com>
2021-07-10 18:36:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,74 @@
 import random as r
 word_list = ['apple', 'mango', 'litchi', 'banana', 'pear']
 chosen_words = r.choice(word_list)
+logo = ''' 
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/    '''
+man = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+print(logo)
 print(chosen_words)
+stage = 6
 length = len(chosen_words)
 display = []
 for i in range(length):
@@ -15,8 +82,14 @@ while not end:
         else:
             pass
     print(display)
+    if guess not in chosen_words:
+        stage -= 1
+        if stage == 0:
+            end = True
+
+            print("You Lose.")
     if "_" not in display:
         end = True
         print("You Win")
-    else:
-        pass
+    print(man[stage])
+
